d5aaab84b2652f9d0ca3fd4ba0861eab21b0ec6b
JesusMartinezClavel <jmcvalles@gmail.com>
2024-02-01 22:14:06 +0100

    made from reto 01 to reto 06

---
+++ js/main.js
@@ -0,0 +1,76 @@
+
+// // Reto 01
+// // 1. Declara 2 variables numéricas (con el valor que desees), he indica cual es mayor de los
+// // dos. Si son iguales indicarlo también. Ves cambiando los valores para comprobar que
+// // funciona.
+
+
+// var num1 = 5
+// var num2 = 5
+
+// num1==num2
+//     ? console.log("iguales")
+//     : num1>num2
+//         ? console.log("1 mayor")
+//         : console.log("2 mayor")
+
+
+// // Reto 02
+// // 2. Declara un String que contenga tu nombre, después muestra un mensaje de bienvenida
+// // por consola. Por ejemplo: si introduzco “Fernando”, me aparezca “Bienvenido Fernando”.
+
+
+// var nombre="Chuso"
+
+
+
+// // Reto 03
+// // 3. Modifica la aplicación anterior, para que nos pida el nombre que queremos introducir
+// (recuerda usar prompt).
+
+// var nombre=prompt("introduce tu nombre")
+
+// console.log(nombre)
+
+
+// Reto04
+// 4. Haz una aplicación que calcule el área de un círculo (pi*R2). El radio se pedirá por teclado
+// (recuerda pasar de String a double con parseDouble). Usa la constante PI.
+
+// const pi=3.14
+
+// var radio = parseInt(prompt("dime un radio7"))
+
+// var area = pi*radio**2
+
+// console.log(area)
+
+
+// Reto 05
+// 5. Lee un número por teclado e indica si es divisible entre 2 (resto = 0). Si no lo es, también
+// debemos indicarlo.
+
+// var numero=parseInt(prompt("introduce un número"))
+
+// if (numero%2==04) {
+//     console.log("es divisible entre 2")
+// }
+// else {
+//     console.log("no es divisible entre 2")
+// }
+
+
+// Reto 06
+// 6. Lee un número por teclado que pida el precio de un producto (puede tener decimales) y
+// calcule el precio final con IVA. El IVA será una constante que sera del 21%
+
+// var numero=parseFloat(prompt("introduce un número"))
+// const IVA=0.21
+
+// precio = numero*(numero*IVA)
+
+// console.log(precio)
+
+
+// Reto 07
+// 7. Muestra los números del 1 al 100 (ambos incluidos). Usa un bucle while.
