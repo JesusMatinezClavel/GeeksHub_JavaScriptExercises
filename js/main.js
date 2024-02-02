6f55fa248496f4ae1acf20272f38d8dc4687d8f8
JesusMartinezClavel <jmcvalles@gmail.com>
2024-02-02 03:35:47 +0100

    I've made the assignments from 7 to 10

--- js/main.js
+++ js/main.js
@@ -74,3 +74,81 @@
 
 // Reto 07
 // 7. Muestra los números del 1 al 100 (ambos incluidos). Usa un bucle while.
+
+// let num = 100
+
+// while (num > 0) {
+//     console.log(num)
+//     num -= 1;
+// }
+
+
+// 8. Haz el mismo ejercicio anterior con un bucle for.
+
+// let num = 100
+
+// for (num;num>0;num--){
+//     console.log(num)
+// }
+
+
+// 9. Muestra los números del 1 al 100 (ambos incluidos) divisibles entre 2 y 3. Utiliza el bucle
+// que desees.
+
+
+// let num = 10
+
+
+// for (num;num>0;num--){
+//     if ((num%2==0)||(num%3==0)){
+//     console.log(num)}}
+
+
+// 10. Realiza una aplicación que nos pida un número de ventas a introducir, después nos
+// pedirá tantas ventas por teclado como número de ventas se hayan indicado. Al final
+// mostrará la suma de todas las ventas. Piensa que es lo que se repite y lo que no.
+
+// let ventasIn = parseInt(prompt("dime cuantas ventas has tenido"))
+
+// var suma=0
+
+
+// for (ventasIn;ventasIn>0;ventasIn--){
+//     valorVentas = parseInt(prompt("dime el precio"))
+//     if (!isNaN(valorVentas)){
+//         suma += valorVentas
+//     }
+// }
+// console.log(suma)
+
+let edad = 16;
+let mayorEdad;
+
+
+mayorEdad = (edad >= 18)
+    ? console.log("si")
+    : (edad=16)
+         ? console.log("16si")
+         : console.log("nop")
+
+
+
+// 11. Crea una aplicación que nos pida un día de la semana y que nos diga si es un día laboral
+// o no. Usa un switch para ello.
+
+// 12. Escribe una aplicación con un String que contenga una contraseña cualquiera. Después
+// se te pedirá que introduzcas la contraseña, con 3 intentos. Cuando aciertes ya no pedirá
+// más la contraseña y mostrará un mensaje diciendo “Enhorabuena”. Piensa bien en la
+// condición de salida (3 intentos y si acierta sale, aunque le queden intentos).
+
+
+// 13. Crea una aplicación llamada CalculadoraInversa, nos pedirá 2 operandos (int) y un signo
+// aritmético (String), según este último se realizará la operación correspondiente. Al final
+// mostrará el resultado en un cuadro de diálogo.
+// Los signos aritméticos disponibles son:
+// +: suma los dos operandos.
+// -: resta los operandos.
+// *: multiplica los operandos.
+// /: divide los operandos, este debe dar un resultado con decimales (double)
+// ^: 1o operando como base y 2o como exponente.
+// %: módulo, resto de la división entre operando1 y operando2.
